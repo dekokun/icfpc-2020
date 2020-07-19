bb9e40ce592e5cc12e743ad85f841581106dedc6
dekokun <dekokun@hatena.ne.jp>
2020-07-19 16:47:24 +0900

    separate send fn

--- app/main.rs
+++ app/main.rs
@@ -1,5 +1,5 @@
 use http_body::Body as _;
-use hyper::{Client, Request, Method, Body, StatusCode};
+use hyper::{Body, Client, Method, Request, StatusCode};
 use std::env;
 use std::process;
 
@@ -11,37 +11,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let player_key = &args[2];
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
+    send(server_url, format!("{}", player_key)).await
+}
 
+async fn send(
+    server_url: &str,
+    body: String,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new();
     let req = Request::builder()
         .method(Method::POST)
         .uri(server_url)
-        .body(Body::from(format!("{}", player_key)))?;
+        .body(Body::from(body))?;
 
     match client.request(req).await {
-        Ok(mut res) => {
-            match res.status() {
-                StatusCode::OK => {
-                    print!("Server response: ");
-                    while let Some(chunk) = res.body_mut().data().await {
-                        match chunk {
-                            Ok(content) => println!("{:?}", content),
-                            Err(why) => println!("error reading body: {:?}", why)
-                        }
+        Ok(mut res) => match res.status() {
+            StatusCode::OK => {
+                print!("Server response: ");
+                while let Some(chunk) = res.body_mut().data().await {
+                    match chunk {
+                        Ok(content) => println!("{:?}", content),
+                        Err(why) => println!("error reading body: {:?}", why),
                     }
-                },
-                _ => {
-                    println!("Unexpected server response:");
-                    println!("HTTP code: {}", res.status());
-                    print!("Response body: ");
-                    while let Some(chunk) = res.body_mut().data().await {
-                        match chunk {
-                            Ok(content) => println!("{:?}", content),
-                            Err(why) => println!("error reading body: {:?}", why)
-                        }
-                    }
-                    process::exit(2);
                 }
+            }
+            _ => {
+                println!("Unexpected server response:");
+                println!("HTTP code: {}", res.status());
+                print!("Response body: ");
+                while let Some(chunk) = res.body_mut().data().await {
+                    match chunk {
+                        Ok(content) => println!("{:?}", content),
+                        Err(why) => println!("error reading body: {:?}", why),
+                    }
+                }
+                process::exit(2);
             }
         },
         Err(err) => {
