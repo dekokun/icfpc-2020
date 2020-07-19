9b25857f03c37f9a936b20cf8b4d515259aa796c
dekokun <dekokun@hatena.ne.jp>
2020-07-20 06:31:19 +0900

    use detonate for examine #release

--- app/main.rs
+++ app/main.rs
@@ -33,8 +33,8 @@ fn make_start_request(player_key: &str, x0: i64, x1: i64, x2: i64, x3: i64) -> S
 }
 
 fn make_commands_request(player_key: &str, ship_id: i64) -> String {
-    format!("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}", mod_str("("), mod_int(4), mod_str(","), mod_int(player_key.parse().unwrap()), mod_str(","), mod_str("("),mod_str("("), mod_int(0), mod_str(","), mod_int(ship_id), mod_str(","), mod_str("("), mod_int(1), mod_str(","), mod_int(1), mod_str(")"),mod_str(")"),mod_str(")"),mod_str(")"))
-    // format!("{}{}{}{}{}{}{}{}{}{}{}", mod_str("("), mod_int(4), mod_str(","), mod_int(player_key.parse().unwrap()), mod_str(","), mod_str("("), mod_int(1), mod_str(","), mod_int(ship_id),mod_str(")"),mod_str(")"))
+    // format!("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}", mod_str("("), mod_int(4), mod_str(","), mod_int(player_key.parse().unwrap()), mod_str(","), mod_str("("),mod_str("("), mod_int(0), mod_str(","), mod_int(ship_id), mod_str(","), mod_str("("), mod_int(1), mod_str(","), mod_int(1), mod_str(")"),mod_str(")"),mod_str(")"),mod_str(")"))
+    format!("{}{}{}{}{}{}{}{}{}{}{}{}{}", mod_str("("), mod_int(4), mod_str(","), mod_int(player_key.parse().unwrap()), mod_str(","), mod_str("("),mod_str("("), mod_int(1), mod_str(","), mod_int(ship_id),mod_str(")"),mod_str(")"),mod_str(")"))
 }
 
 
