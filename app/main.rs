fe7d168b9e608f81a84c1dede96f53caac3e9121
dekokun <dekokun@hatena.ne.jp>
2020-07-20 18:14:33 +0900

    use server response

--- app/main.rs
+++ app/main.rs
@@ -19,10 +19,18 @@ async fn main() {
     let x2: i64 = 0;
     let x3: i64 = 1;
     let _game_response = send(server_url, make_join_request(player_key)).await;
-    let _game_response = send(server_url, make_start_request(player_key, x0, x1, x2, x3)).await;
+    let (_game_stage, _role, ship_id, _position_x, _position_y, velocity_x, velocity_y) =
+        send(server_url, make_start_request(player_key, x0, x1, x2, x3))
+            .await
+            .unwrap();
     loop {
-        let _game_response = send(server_url, make_commands_request(player_key, 1)).await;
-        let _game_response = send(server_url, make_commands_request(player_key, 0)).await;
+        let (_game_stage, _role, _ship_id, _position_x, _position_y, velocity_x, velocity_y) =
+            send(
+                server_url,
+                make_commands_request(player_key, ship_id, velocity_y, velocity_y),
+            )
+            .await
+            .unwrap();
     }
 }
 
@@ -60,7 +68,12 @@ fn make_start_request(player_key: &str, x0: i64, x1: i64, x2: i64, x3: i64) -> S
     )
 }
 
-fn make_commands_request(player_key: &str, ship_id: i64) -> String {
+fn make_commands_request(
+    player_key: &str,
+    ship_id: i64,
+    velocity_x: i64,
+    velocity_y: i64,
+) -> String {
     format!(
         "{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
         mod_str("("),
@@ -75,8 +88,8 @@ fn make_commands_request(player_key: &str, ship_id: i64) -> String {
         mod_int(ship_id),
         mod_str(","),
         mod_str("cons"),
-        mod_int(1),
-        mod_int(1),
+        mod_int(velocity_x),
+        mod_int(velocity_y),
         mod_str(")"),
         mod_str(")"),
         mod_str(")")
@@ -171,14 +184,132 @@ fn decode_response(s: &str) -> String {
         "11" => "(".to_owned() + &decode_response(&s[2..s.len() - 2]) + ")",
         "10" | "01" => {
             let (num, remain) = decode_int(&s);
-            num + &decode_response(remain)
+            num.to_string() + &decode_response(remain)
         }
         "00" => "nil".to_owned() + &decode_response(&s[2..]),
         _ => unimplemented!(),
     }
 }
 
-fn decode_int(mut s: &str) -> (String, &str) {
+fn decode_response2(s: &str) -> (i64, i64, i64, i64, i64, i64, i64) {
+    let s = expect_token(s, "(");
+    let (one, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (game_stage, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    // staticGameInfo start
+    let s = expect_token(s, "(");
+    let (static_x0, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (role, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    // x2 start
+    let s = expect_token(s, "(");
+    let (static_x2_0, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (static_x2_1, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (static_x2_2, s) = decode_int(s);
+    let s = expect_token(s, ")");
+    let s = expect_token(s, ",");
+    // x3 start
+    let s = expect_token(s, "(");
+    let (static_x3_0, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (static_x3_1, s) = decode_int(s);
+    let s = expect_token(s, ")");
+    let s = expect_token(s, ",");
+    // x4 is nil or (x, x, x, x)
+    // consume nill or
+    let (mut s, is_not_nil) = consume_token(s, "(");
+    if is_not_nil {
+        let (static_x4_0, s2) = decode_int(s);
+        s = s2;
+        s = expect_token(s, ",");
+        let (static_x4_1, s2) = decode_int(s);
+        s = s2;
+        s = expect_token(s, ",");
+        let (static_x4_2, s2) = decode_int(s);
+        s = s2;
+        s = expect_token(s, ",");
+        let (static_x4_3, s2) = decode_int(s);
+        s = expect_token(s, ")");
+    } else {
+        s = expect_token(s, "nil");
+    }
+    let s = expect_token(s, ")");
+    let s = expect_token(s, ",");
+    // gameState start
+    // consume nill or
+    let (mut s, is_nil) = consume_token(s, "nil");
+    if is_nil {
+        return (game_stage, role, 0, 0, 0, 0, 0);
+    }
+    let s = expect_token(s, "(");
+    let (game_tick, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    // x1 start
+    let s = expect_token(s, "(");
+    let (state_x1_0, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (state_x1_1, s) = decode_int(s);
+    let s = expect_token(s, ")");
+    let s = expect_token(s, ",");
+    // shipsAndCommands start
+    let s = expect_token(s, "(");
+    let s = expect_token(s, "(");
+    let s = expect_token(s, "(");
+    let (role, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let (ship_id, s) = decode_int(s);
+    let s = expect_token(s, "cons");
+    // よくわからんがもう一度受けてみる。
+    let s = expect_token(s, "cons");
+    let (position_x, s) = decode_int(s);
+    let (position_y, s) = decode_int(s);
+    let s = expect_token(s, ",");
+    let s = expect_token(s, "cons");
+    let (velocity_x, s) = decode_int(s);
+    let (velocity_y, _s) = decode_int(s);
+    dbg!(
+        game_stage,
+        static_x0,
+        role,
+        static_x2_0,
+        static_x2_1,
+        static_x2_2,
+        static_x3_0,
+        static_x3_1,
+        game_tick,
+        ship_id,
+        position_x,
+        position_y
+    );
+    dbg!(state_x1_0, state_x1_1,);
+    (
+        game_stage, role, ship_id, position_x, position_y, velocity_x, velocity_y,
+    )
+}
+
+fn consume_token<'a>(s: &'a str, token: &str) -> (&'a str, bool) {
+    let expect_raw = mod_str(token);
+    let ret = &s[..2] == expect_raw;
+    if ret {
+        (&s[2..], ret)
+    } else {
+        (s, ret)
+    }
+}
+fn expect_token<'a>(s: &'a str, token: &str) -> &'a str {
+    let expect_raw = mod_str(token);
+    assert_eq!(&s[..2], expect_raw);
+    &s[2..]
+}
+
+fn expect_int(s: &str) {
+    assert!(&s[..2] == "01" || &s[..2] == "10");
+}
+fn decode_int(mut s: &str) -> (i64, &str) {
     let minus = match &s[0..2] {
         "10" => true,
         "01" => false,
@@ -203,13 +334,13 @@ fn decode_int(mut s: &str) -> (String, &str) {
         ans += c.to_digit(10).unwrap() as i64 * 2_i64.pow(i as u32);
     }
     ans = if minus { -ans } else { ans };
-    (ans.to_string(), &s[size..])
+    (ans, &s[size..])
 }
 
 async fn send(
     server_url: &str,
     body: String,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<(i64, i64, i64, i64, i64, i64, i64), Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new();
     println!("body: {}", body);
     let req = Request::builder()
@@ -225,9 +356,11 @@ async fn send(
                 while let Some(chunk) = res.body_mut().data().await {
                     match chunk {
                         Ok(content) => {
+                            println!("{:?}", content);
                             let st = &format!("{:?}", content);
                             // debug出力(b"で始まり"で終わる)を迂回…
-                            println!("{:?}", decode_response(&st[2..st.len() - 1]))
+                            let st = &st[2..st.len() - 1];
+                            return Ok(decode_response2(st));
                         }
                         Err(why) => println!("error reading body: {:?}", why),
                     }
@@ -252,7 +385,7 @@ async fn send(
         }
     }
 
-    Ok(())
+    Ok((0, 0, 0, 0, 0, 0, 0))
 }
 
 #[cfg(test)]
@@ -328,8 +461,25 @@ mod tests {
     #[test]
     fn decode_test() {
         assert_eq!(
-            decode_response("1101100001110110000111110111100001000000001101011110111100010000000001101100001110111001000000001111011100001000011011101000000000111101101010110110101011011010101101101010000011110101111011100001000011011101000000000111111110110000111010111110100111101100011000011110100101111011010101101101010110110101011011010100011010110111001000000110110000100110000111111010110110000111110110011101110001100001111010010111101111000011011101011011000011101011011000010011010110111001000000110110000100110000000000"),
-            "(1(1((256(0((512(1(64nil((16(128nil((10(10(10(10nilnil((0((16(128nil((((1(0((-7-48((00((10(10(10(10nil(0(64(1nil(nilnil(((0(1((748((0))))))))))))))))))))))))))))))))))))))))))))))))".to_owned()
+            // https://icfpc2020-api.testkontur.ru/logs?logKey=uHK5ukFTb3Z0X%2F7EbI08ZtEdR%2FT6pFYyxMkOlWTufuc%3D&apiKey=aeabd1ef5a0c4a0f98041be9c1ad59fd
+            // https://icfpcontest2020.github.io/#/visualize?game=7652258d-b953-4911-ab44-393279550470
+            decode_response("11011000011101100001111101111000010000000011011000011111011110000111000000110110000111011100100000000111101110000100001101110100000000011000011110110001011110111000010000110111010000000001111111101100001110101111101010011011000101110111110100001011000011111011110000110111001110110000111010110110000100110110100011011100100000011011000010011111101011110110000101100001000000111111010110110000111110110100001110001100001111010010111101110111111101101101010110110101011011000010011010110111001000000110110000100111111010111101010100001000000000000"),
+            "(1(1((256(1((448(1(64nil((16(128nil(nilnil((2((16(128nil((((1(0((-9-46((-11((441(1(0(1nil(8(64(1nil(((0((11nilnilnil(((0(1((848((00((254))))))))))))))))))))))))))))))))))))))))))))))))))".to_owned()
+            // "(1,1:gateStage,(256:x0,0:role,(512,1,64):x2,(16,128):x3,(10,10,10,10):x4):staticGameInfo,(0: gameTick,(16,128): x1,(((1:role,0:shipId,cons -7 -48:position,cons 0 0: velocity,(10,10,10,10):x4,0:x5,64:x6,1:x7)(nilnil(((0(1((748((0))))))))))))))))))))))))))))))))))))))))))))))))".to_owned()
+            // "(1,1:gameStage,(256:x0,1:role,(448,1,64):x2,(16,128):x3,nil:x4):staticGameInfo,(2:gameTick,(16,128):x1,(((1(0((-9-46((-11((441(1(0(1nil(8(64(1nil(((0((11nilnilnil(((0(1((848((00((254))))))))))))))))))))))))))))))))))))))))))))))))))".to_owned()
         )
+    }
+    #[test]
+    fn decode_response2_test() {
+        assert_eq!(
+            decode_response2("110110000111011000011111011110000110000000110110000111110111100001110000001101100001110111001000000001111011100001000011011101000000000110000111101011110111000010000110111010000000001111111101100001110101111011100010000001110001100001111010010111101111000011011101011011000011101011011000010011010110111001000000110110000100110000111111010110110000111111011000100000101100011000011110100101111011000011101100010110110001111011001000011010110111001000000110110000100110000000000"), (1,1,0,0,0,0,0)
+        );
+    }
+    #[test]
+    fn decode_response_test2() {
+        assert_eq!(
+            decode_response("110110000111011000011111011110000110000000110110000111110111100001110000001101100001110111001000000001111011100001000011011101000000000110000111101011110111000010000110111010000000001111111101100001110101111011100010000001110001100001111010010111101111000011011101011011000011101011011000010011010110111001000000110110000100110000111111010110110000111111011000100000101100011000011110100101111011000011101100010110110001111011001000011010110111001000000110110000100110000000000"), ""
+        );
+        // (1,1,(384,1,(448,1,64),(16,128),nil),(0,(16,128),(((1(0((3248((00((442(1(0(1nil(0(64(1nil(nilnil(((0(1((-32-48((00)))))))))))))))))))))))))))))))))))))))))))
     }
 }
